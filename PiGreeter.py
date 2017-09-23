fae27bd7ceb5cf8ae53aa12ec8880a92c9614143
Hubert Woszczyk <woszczykh@gmail.com>
2017-09-23 23:14:44 +0200

    Reorganized database, added code for recognition

--- PiGreeter.py
+++ PiGreeter.py
@@ -4,9 +4,15 @@ import wave
 import numpy as np
 import cv2
 import cv2.face as face
-import pyaudio
+import os
+import re
+#import pyaudio
 
-def detect_faces(grayscale, cascade):
+def detect_eyes(cascade, grayscale):
+    "Detects eyes in a grayscale image"
+    pass
+
+def detect_faces(cascade, grayscale):
     "Detects faces using a haarcascade in a grayscale image"
     return cascade.detectMultiScale(
         grayscale,
@@ -14,14 +20,49 @@ def detect_faces(grayscale, cascade):
         minNeighbors=5
     )
 
+def load_images_labels(path, cascade):
+    "Loads images from dataset"
+
+    images_paths = [os.path.join(path, f) for f in os.listdir(path)]
+
+    images = []
+    labels = []
+    label = -1
+    personName = ""
+    for image_path in images_paths:
+        img = cv2.imread(image_path)
+        if os.path.split(image_path)[1].split('_')[0] != personName:
+            label = label + 1
+        #label = int( re.findall(r'\d+', os.path.split(image_path)[1]) )
+        faces = detect_faces(cascade, img)
+        for (x, y, w, h) in faces:
+            image_resized = cv2.resize( img[y: y + h, x: x + w], (128, 128) )
+            image_resized = cv2.cvtColor(image_resized, cv2.COLOR_BGR2GRAY)
+            images.append(image_resized)
+            labels.append(label)
+            cv2.imshow("Adding faces to training set...", image_resized)
+            cv2.waitKey(500)
+
+    return images, labels
+
+def process_faces(faces, img):
+    "Crops image to separates faces"
+    i = 0
+    f = []
+    for (x, y, w, h) in faces:
+        f.append( img[y:y + h, x:x + w] )
+    
+    return f
+
 def draw_faces(faces, img):
     "Draws rectanges around faces"
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-def load_images():
-    "Loads images for the recognizer module"
-    pass
+def recognize_faces(faces, recognizer, frame):
+    "Recognize faces in a list and reacts"
+    for (x, y , w, h) in faces:
+        nbr_predicted, conf = recognizer.predict(frame[y:y+h, x:x+h])
 
 if __name__ == "__main__":
     """ main
@@ -32,29 +73,13 @@ if __name__ == "__main__":
         4. Recognize faces (LBPH)
         5. Output appropriate greeting (pyaudio ? espeek ?)
     """
-    print(help(cv2.face))
-    cap = cv2.VideoCapture("faceDetection.mp4")
+    cap = cv2.VideoCapture(0)
     face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
     recognizer = face.createLBPHFaceRecognizer()
 
-    wf = wave.open("audio/speech.wav", 'rb')
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True)
-
-    data = wf.readframes(1024)
-
-    while len(data) > 0:
-        stream.write(data)
-        data = wf.readframes(1024)
-
-    stream.stop_stream()
-    stream.close()
-
-    p.terminate()
+    # Train
+    images, labels = load_images_labels("./trainingData", face_cascade)
+    recognizer.train( images, np.array(labels) )
 
     while True:
         ret, frame = cap.read()
@@ -62,10 +87,22 @@ if __name__ == "__main__":
             print("End of file")
             break
         else:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if 3 == len(frame.shape):
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            elif 4 == len(frame.shape):
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY)
+            else:
+                gray = frame
+        
+        # Histogram equalization - consider using CLAHE if does not work well
+        gray = cv2.equalizeHist(gray)
 
-        faces = detect_faces(gray, face_cascade)
+        # Add flag to only look for one face at a time ? 
+        faces = detect_faces(face_cascade, gray)
         draw_faces(faces, frame)
+
+        # Recognize !
+        recognize_faces(faces, recognizer, frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
